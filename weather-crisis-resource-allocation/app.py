304c4d01b4c9d9f48cc3a3d3d860c3d25787485f
DragonsOfTarkir <ianjgrinarml@icloud.com>
2025-12-05 18:23:09 -0500

    Update app.py

--- weather-crisis-resource-allocation/app.py
+++ weather-crisis-resource-allocation/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, render_template
 from models.data_models import Region, Depot
 from algorithms.greedy import greedy_allocation
 from algorithms.knapsack import knapsack_allocate
-from algorithms.maxflow import max_flow
 
 app = Flask(__name__)
 
@@ -33,18 +32,6 @@ def knapsack_api():
 
     result = knapsack_allocate(regions, depot.supply)
     return jsonify(result)
-
-
-@app.route("/route/maxflow", methods=["POST"])
-def maxflow_api():
-    data = request.json
-
-    capacity = data["capacity"]
-    source = data["source"]
-    sink = data["sink"]
-
-    result = max_flow(capacity, source, sink)
-    return jsonify({"method": "max_flow", "max_flow": result})
 
 
 if __name__ == "__main__":
