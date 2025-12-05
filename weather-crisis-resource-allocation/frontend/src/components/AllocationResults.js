5925add2f7cb6e25f0c32cc330ee5d92e4c881a0
DragonsOfTarkir <ianjgrinarml@icloud.com>
2025-12-05 12:01:15 -0500

    Update AllocationResults.js

--- weather-crisis-resource-allocation/frontend/src/components/AllocationResults.js
+++ weather-crisis-resource-allocation/frontend/src/components/AllocationResults.js
@@ -1,1 +1,9 @@
+export default function AllocationResults({ results }) {
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h2>Results</h2>
+      <pre>{JSON.stringify(results, null, 2)}</pre>
+    </div>
+  );
+}
 
