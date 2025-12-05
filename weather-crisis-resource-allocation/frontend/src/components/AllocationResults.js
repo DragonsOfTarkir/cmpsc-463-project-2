7b6cc29ded841fbac5ddba7045f887e05f371172
FavianaT <favianat802@gmail.com>
2025-12-05 13:55:54 -0500

    -

--- weather-crisis-resource-allocation/frontend/src/components/AllocationResults.js
+++
@@ -1,9 +0,0 @@
-export default function AllocationResults({ results }) {
-  return (
-    <div style={{ marginTop: "2rem" }}>
-      <h2>Results</h2>
-      <pre>{JSON.stringify(results, null, 2)}</pre>
-    </div>
-  );
-}
-
